86a1e33d8ae74d83c54926a4497a2f432cebe7d9
Jose Galvez <chemagalveze@gmail.com>
2023-07-08 14:21:58 -0600

    Trying to show a plot

--- notebooks/app.py
+++ notebooks/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Website introduction/information
 st.markdown ("""
@@ -11,3 +12,8 @@ st.markdown ("""
 
 # Upload DataFrame
 df = pd.read_csv('indicadores de pobreza municipal_2010.csv')
+
+X = df['total_population']
+y = df['poverty_percentage']
+
+plt.plot(X,y)
