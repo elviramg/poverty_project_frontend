86d17b0d647456f622482afb24f9ed5584572912
Jose Galvez <chemagalveze@gmail.com>
2023-07-11 20:08:09 -0600

    add tabs

--- notebooks/app.py
+++ notebooks/app.py
@@ -3,12 +3,30 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Create tabs
+tabs = ["Tab 1", "Tab 2", "Tab 3"]
+active_tab = st.sidebar.radio("Select Tab", tabs)
+
+# Tab 1
+if active_tab == "Tab 1":
+    st.title("Tab 1 Content")
+
+
+# Tab 2
+elif active_tab == "Tab 2":
+    st.title("Tab 2 Content")
+
+
+# Tab 3
+elif active_tab == "Tab 3":
+    st.title("Tab 3 Content")
+
+
 # Website introduction/information
 st.markdown ("""
              # Header
              ## Subheader
              Normal Text""")
-
 
 # Upload DataFrame
 df = pd.read_csv('data/indicadores de pobreza municipal_2010.csv', encoding='latin-1')
