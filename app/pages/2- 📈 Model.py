fe8ce57d90196ed8d59d9255cd268cb8502c2d83
Jose Galvez <chemagalveze@gmail.com>
2023-07-29 12:06:36 -0600

    Add dotted line

--- app/pages/2- 📈 Model.py
+++ app/pages/2- 📈 Model.py
@@ -12,7 +12,7 @@ data = get_csv()
 data_model = get_model_csv()
 merged_data = pd.concat([data, data_model])
 
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 #adding marging specs for the main page with css inyection
 margins_css = """
@@ -42,7 +42,7 @@ with col1:
         )
     )
 
-    def line_plots(data: pd.DataFrame):
+    def line_plots(data: pd.DataFrame, data_model: pd.DataFrame):
         """Renders line plots for selected regions (states) from data argument."""
 
         # Get the state names (column names) from the DataFrame
@@ -57,26 +57,44 @@ with col1:
 
         # Filter data for the selected states
         selected_states = data[states]
+        selected_states_dotted = data_model[states]
 
         if selected_states.empty:
             st.warning("No state selected!")
         else:
             # Create a DataFrame for the selected states and reset the index
             selected_states_df = selected_states.reset_index()
+            selected_states_df_dotted = selected_states_dotted.reset_index()
 
             # Convert the datetime values in the index to strings
             selected_states_df["index"] = selected_states_df["index"].dt.strftime("%Y-%m-%d")
+            selected_states_df_dotted["index"] = selected_states_df_dotted["index"].dt.strftime("%Y-%m-%d")
 
             # Melt the DataFrame to make it suitable for Altair
             selected_states_melted = pd.melt(selected_states_df, id_vars="index", var_name="State", value_name="Percentage")
+            selected_states_melted_dotted = pd.melt(selected_states_df_dotted, id_vars="index", var_name="State", value_name="Percentage")
 
-            # Create the Altair chart
+            # Create the Altair chart without configurations
             chart = alt.Chart(selected_states_melted).mark_line().encode(
                 x=alt.X("index:T", title="Date"),
                 y=alt.Y("Percentage:Q", title="Percentage"),
                 color=alt.Color("State:N", title="State", scale=alt.Scale(scheme="category10")),
                 tooltip=["index:T", "Percentage:Q", "State:N"]
             ).properties(
+                width=550,
+                height=400
+            )
+
+            # Create Dotted data without configurations
+            chart_dotted = alt.Chart(selected_states_melted_dotted).mark_line(strokeDash=[5, 5]).encode(
+                x=alt.X("index:T"),
+                y=alt.Y("Percentage:Q"),
+                color=alt.Color("State:N", scale=alt.Scale(scheme="category10")),
+                tooltip=["index:T", "Percentage:Q", "State:N"]
+            )
+
+            # Combine both charts and then add configurations
+            combined_chart = alt.layer(chart, chart_dotted).properties(
                 width=550,
                 height=400
             ).configure_axis(
@@ -89,13 +107,13 @@ with col1:
                 color="white"
             ).interactive()
 
-
-        # Display the Altair chart using Streamlit's altair_chart function
-        st.altair_chart(chart)
+            # Display the Altair chart using Streamlit’s altair_chart function
+            st.altair_chart(combined_chart)
 
     # Call the function with the 'data' DataFrame as the argument
     data = get_csv()
-    line_plots(data)
+    data_model = get_model_csv()
+    line_plots(data, data_model)
 
 with col2:
     st.markdown(("## " + ("Poverty Rate by State in Mexico")))
