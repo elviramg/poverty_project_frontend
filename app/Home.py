55176259a5d7387e39ed9d3ba8911794205a58a8
Jose Galvez <chemagalveze@gmail.com>
2023-08-05 12:02:37 -0600

    Text changes provided by Josh and Adrian

--- app/Home.py
+++ app/Home.py
@@ -26,18 +26,16 @@ col1, col2 = st.columns([1,1])
 with col1:
     ## Poverty project Front
     st.markdown("""
-        Poverty poses a significant challenge in the Country, according to CONEVAL's latest data, almost 40% of the country populations lives in poverty.\n
-        So, identifying areas at risk of increasing poverty can help policymakers target interventions effectively. \n
-        This intervention could lead to a better allocation of human and economic resources.
+        Poverty poses a significant challenge to Mexico. According to the latest data from CONEVAL (National Council for the Evaluation of Social Development Policy), nearly 40% of the country’s population lives in some form of poverty ([CONEVAL, 2022](https://www.coneval.org.mx/SalaPrensa/Comunicadosprensa/Documents/2022/COMUNICADO_18_ITLP_3T_2022.pdf)).\n
+        Hence, identifying areas at risk of increasing poverty may prove helpful for policymakers to take effective steps towards tackling and preventing such increases. \n
+        This might translate into a better allocation of human and economic resources or selective monitoring of poverty indicators in vulnerable states.
 """)
 
 with col2:
     st.markdown("""
-    Our data comes from Coneval's Labor poverty Trend Index ('ITPL by its acronym in Spanish'). By sharing this information, our main goal it's to understand how labor poverty has evolved over time \n
-
-    We seek to develop our skills in data analysis with a socially responsible approach towards our country.\n
-    Hoping also that this can serve as an aid in the investigation of poverty and helping policymakers to combat it. \n
-    To be transparent from the beginning, you can find the data for our project publicly available [here](https://www.coneval.org.mx/Medicion/Paginas/ITLP-IS_pobreza_laboral.aspx)
+    Our data comes from CONEVAL’s Labor Poverty Trend Index (ITLP by its Spanish acronym). Our main goal is to understand how labor poverty has evolved over time through statistical analysis derived from the data gathered, as well as to provide a benchmark view into future trends utilizing elementary data engineering techniques. \n With this project we seek to develop our skills in data analysis by providing policy makers and community leaders with a resource for making data-driven decisions to better the people of Mexico.
+    We also aim to contribute to further advancement of social research in Mexico and help government officials and institutions implement comprehensive social and economic policies where needed most. \n
+    For full transparency, you can find the public data used for this project [here](https://www.coneval.org.mx/Medicion/Paginas/ITLP-IS_pobreza_laboral.aspx).
     """)
 
-st.image("pob_ingreso_menor_canasta.png", caption="Percentage of the population with labor income lower than the cost of the food basket", use_column_width=True)
+st.image("pob_ingreso_menor_canasta.png", caption="Percentage of the population with labor income lower than the cost of the basic food basket", use_column_width=True)
